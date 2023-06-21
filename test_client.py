0895f8ea4945dda1da283b14dcc29923c68ef7df
bruno <brunodecruz.05@gmail.com>
2023-06-21 16:39:05 -0300

    Docstrings, typehints and 2d graph

--- test_client.py
+++ test_client.py
@@ -4,65 +4,95 @@ import math
 import time
 pi = math.pi
 
-    
-def find_entry_with_cima(data):
-    for territory, territory_values in data.items():
-        for entry, entry_values in territory_values.items():
-            if entry_values.get('cima', False):
-                return entry
+def encontrar_entrada_con_cima(data: dict) -> bool:
+    """
+    Encuentra la entrada con el valor 'cima' establecido en True en los datos proporcionados.
+
+    Args:
+        data (dict): Datos que contienen territorios y entradas.
+
+    Returns:
+        bool: True si se encuentra una entrada con el valor 'cima', False de lo contrario.
+    """
+    for territorio, valores_territorio in data.items():
+        for entrada, valores_entrada in valores_territorio.items():
+            if valores_entrada.get('cima', False):
+                return entrada
     return False
-def list_with_z (data ,escaladoor):
+
+
+def lista_con_z(data: dict, escalador: str) -> list:
+    """
+    Crea una lista de valores 'z' para todos los escaladores excepto el especificado.
+
+    Args:
+        data (dict): Datos que contienen territorios y entradas.
+        escalador (str): Nombre del escalador.
+
+    Returns:
+        list: Lista de valores 'z' para los demás escaladores.
+    """
     respuesta = []
-    for equipo in data :
-        for escladores in data[equipo]:
-            if escladores != escalador:
-                respuesta.append(data[equipo][escladores]['z'])
+    for equipo in data:
+        for escaladores in data[equipo]:
+            if escaladores != escalador:
+                respuesta.append(data[equipo][escaladores]['z'])
     return respuesta
-#c= MountainClient('10.42.0.1',8888)
-c=MountainClient()
-c.add_team('T1', ['E1','E2','E3','E4'])
-E1=Escalador("E1",1)
-E2=Escalador("E2",2)
-E3=Escalador("E3",3)
-E4=Escalador("E4",4)
+
+
+c = MountainClient()
+
+# Agregar equipos y escaladores
+
+c.add_team('T1', ['E1', 'E2', 'E3', 'E4'])
+E1 = Escalador("E1", 1)
+E2 = Escalador("E2", 2)
+E3 = Escalador("E3", 3)
+E4 = Escalador("E4", 4)
+
 c.finish_registration()
-cuadrante_division=False
+
 while not c.is_over():
-    speed = 50
+    velocidad = 50
     lista_escaladores = [E1, E2, E3, E4]
     direccion = {}
     data = c.get_data()
-    #print(data)
-    cima=find_entry_with_cima(data)
+    cima = encontrar_entrada_con_cima(data)
     time.sleep(0)
-    if cima== False:
-            for escalador in lista_escaladores:
-                 z_escladores= list_with_z(data,escalador.nombre)
-                 direccion[escalador.nombre] = {'direction': escalador.calculate_direction(data['T1'][escalador.nombre]['inclinacion_x'],
-                                                data['T1'][escalador.nombre]['inclinacion_y'],
-                                                data['T1'][escalador.nombre]['x'],
-                                                data['T1'][escalador.nombre]['y'],
-                                                data['T1'][escalador.nombre]['z'],
-                                                z_escladores),
-                                                'speed' : speed} 
-    else:
 
+    if cima is False:
         for escalador in lista_escaladores:
-            if escalador.nombre ==cima:
-                direccion[escalador.nombre] = {'direction': escalador.calculate_direction(data['T1'][escalador.nombre]['inclinacion_x'],
-                                                data['T1'][escalador.nombre]['inclinacion_y'],
-                                                data['T1'][escalador.nombre]['x'],
-                                                data['T1'][escalador.nombre]['y'],
-                                                data['T1'][escalador.nombre]['z'],
-                                                z_escladores),
-                                                'speed' : speed} 
+            z_escaladores = lista_con_z(data, escalador.nombre)
+            direccion[escalador.nombre] = {
+                'direction': escalador.calcular_direccion(
+                    data['T1'][escalador.nombre]['inclinacion_x'],
+                    data['T1'][escalador.nombre]['inclinacion_y'],
+                    data['T1'][escalador.nombre]['x'],
+                    data['T1'][escalador.nombre]['y'],
+                    data['T1'][escalador.nombre]['z'],
+                    z_escaladores),
+                'speed': velocidad}
+    else:
+        for escalador in lista_escaladores:
+            if escalador.nombre == cima:
+                direccion[escalador.nombre] = {
+                    'direction': escalador.calcular_direccion(
+                        data['T1'][escalador.nombre]['inclinacion_x'],
+                        data['T1'][escalador.nombre]['inclinacion_y'],
+                        data['T1'][escalador.nombre]['x'],
+                        data['T1'][escalador.nombre]['y'],
+                        data['T1'][escalador.nombre]['z'],
+                        z_escaladores),
+                    'speed': velocidad}
             else:
-                direccion[escalador.nombre] = {'direction': escalador.calcular_angulo(
-                                                                                data['T1'][escalador.nombre]['x'],
-                                                                                data['T1'][escalador.nombre]['y'],
-                                                                                data['T1'][cima]['x'],
-                                                                                data['T1'][cima]['y']
-                                                                               ),
-                                                                               'speed' : speed} 
-                print("Se encontro la cima ", direccion)
-    c.next_iteration('T1',direccion)
+                direccion[escalador.nombre] = {
+                    'direction': escalador.calcular_angulo(
+                        data['T1'][escalador.nombre]['x'],
+                        data['T1'][escalador.nombre]['y'],
+                        data['T1'][cima]['x'],
+                        data['T1'][cima]['y']
+                    ),
+                    'speed': velocidad}
+        print("Se encontró la cima", direccion)
+
+    c.next_iteration('T1', direccion)
